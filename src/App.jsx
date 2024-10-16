e7f1b5e266ca13385bf041ce87c83ba4f4a8c175
Gordon D <gordon.draine97@yahoo.com>
2024-10-16 02:27:48 -0400

    initial commit with adding footer and ideal article display

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Blog />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
